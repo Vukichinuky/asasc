bddeda68515db36a2758feef3ebecdb7c8fd2275
Vukichinuky <gudraliin@gmail.com>
2023-02-09 20:53:34 +0100

    shell update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ab85182770e4efb"; }
+/******/ 		__webpack_require__.h = function() { return "d74ccd02118c560e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
